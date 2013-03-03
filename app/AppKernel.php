ea3a079f8ff5f4a848db0164f2f72c76315311e0
Frederic Haddad <freddy@coddict.com>
2013-03-03 00:37:16 -0500

    Created the DevExtra Bundle and defined a servce

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 	    new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Coddict\DevExtraBundle\CoddictDevExtraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
