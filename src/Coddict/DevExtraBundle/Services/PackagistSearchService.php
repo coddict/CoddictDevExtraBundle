309d4f58af064eb66a1d02f89e1a31b090ed6a8a
abderrahamne benbachir <anis.benbachir@gmail.com>
2013-03-13 02:57:57 -0400

    add packagistSearch service

---
+++ src/Coddict/DevExtraBundle/Services/PackagistSearchService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Coddict\DevExtraBundle\Services;
+use Guzzle\Http\Client;
+
+class PackagistSearchService extends AbstractService
+{
+	protected $url = 'http://packagist.org';
+	
+	protected function package_sorter($key) {
+	    return function ($a, $b) use ($key) {
+	        return strnatcmp($a[$key], $b[$key]);
+	    };
+	}
+
+	public function search($word){
+		$client = new Client($this->url);
+		$request = $client->get('/search.json?q=' . urlencode($word));
+		$data = $request->send()->json()["results"];
+		// sort the packages by downloads
+		usort($data, $this->package_sorter('downloads'));
+		$output = array();
+		foreach($data as $key => $package){
+			$output[] = array(
+				"name"			=> $package["name"],
+				"description"	=> $package["description"],
+				"package"		=> $package["url"],
+			);
+		}
+		return $output;
+	}
+	
+	public function getPackage($name){
+		if(empty($name))
+			throw new \Exception("name is empty");
+		$client = new Client($this->url);
+		$request = $client->get('/packages/'.$name.'.json');
+		$fullPackage = $request->send()->json();
+		if(!array_key_exists("package", $fullPackage) )
+			throw new \Exception("The package was not found");
+		return $fullPackage["package"];
+	}
+}
+
+?>
