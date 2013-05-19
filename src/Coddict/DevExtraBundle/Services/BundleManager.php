14c5904e8550f9998fbbf7cba7076e300da33088
abderrahamne benbachir <anis.benbachir@gmail.com>
2013-05-19 16:17:04 -0400

    add BundleManager service

---
+++ src/Coddict/DevExtraBundle/Services/BundleManager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Coddict\DevExtraBundle\Services;
+
+
+
+class BundleManager extends AbstractService
+{
+    
+    public function getBundles()
+    {
+	    // TODO : return all available bundles
+    }
+        
+}
+
+?>
